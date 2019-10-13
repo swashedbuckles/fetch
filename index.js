a985c53b103adbd943482be01f49e256432aba37
Tom Joseph <swashedbuckles@gmail.com>
2019-10-13 11:27:51 -0400

    hookup response skeleton

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import Request from './lib/request';
+import response from './lib/response';
+
 export {default as Request} from './lib/request';
+export {default as response} from './lib/response';
 
 export default function fetch() {
   return new Promise((resolve, reject) => {});
